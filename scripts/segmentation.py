28981c439f5c27ac428e7fdc9e3cba58a096397d
yulivee <coding@voiding-warranties.de>
2017-07-18 22:02:49 +0200

    (feat) Added Code skeleton for clustering

--- scripts/segmentation.py
+++ scripts/segmentation.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # Import modules
+import pcl
 from pcl_helper import *
 
 # TODO: Define functions as required
@@ -8,26 +9,80 @@ from pcl_helper import *
 # Callback function for your Point Cloud Subscriber
 def pcl_callback(pcl_msg):
 
-    # TODO: Convert ROS msg to PCL data
+    # Convert ROS msg to PCL data
+    cloud = ros_to_pcl(pcl_msg)
 
-    # TODO: Voxel Grid Downsampling
+    # Voxel Grid Downsampling
+    vox = cloud.make_voxel_grid_filter()
+    LEAF_SIZE = 0.01
+    vox.set_leaf_size(LEAF_SIZE, LEAF_SIZE, LEAF_SIZE)
+    cloud_filtered = vox.filter()
 
-    # TODO: PassThrough Filter
+    # PassThrough Filter
+    passthrough = cloud_filtered.make_passthrough_filter()
+    filter_axis = 'z'
+    passthrough.set_filter_field_name(filter_axis)
+    axis_min = 0.6
+    axis_max = 1.1
+    passthrough.set_filter_limits(axis_min, axis_max)
+    cloud_filtered = passthrough.filter()
 
+    # RANSAC Plane Segmentation
+    seg = cloud_filtered.make_segmenter()
 
-    # TODO: RANSAC Plane Segmentation
+    # Extract inliers and outliers
+    seg.set_model_type(pcl.SACMODEL_PLANE)
+    seg.set_method_type(pcl.SAC_RANSAC)
+    max_distance = 0.01
+    seg.set_distance_threshold(max_distance)
 
-    # TODO: Extract inliers and outliers
+    outlier_filter = cloud_in.make_statistical_outlier_filter()
+    outlier_filter.set_mean_k(50)
+    outlier_filter.set_std_dev_mul_thresh(x)
+    cloud_filtered = outlier_filter.filter()
+    
+    inliers, coefficients = seg.segment()
+    extracted_inliers = cloud_filtered.extract(inliers, negative=False)
+    extracted_outliers = cloud_filtered.extract(inliers, negative=True)
 
-    # TODO: Euclidean Clustering
+    cloud_table
+    cloud_objects
 
-    # TODO: Create Cluster-Mask Point Cloud to visualize each cluster separately
+    # Euclidean Clustering
+    white_cloud = # Apply function to convert XYZRGB to XYZ
+    tree = white_cloud.make_kdtree()
+    ec = white_cloud.make_EuclideanClusterExtraction()
+    ec.set_ClusterTolerance(0.001)
+    ec.set_MinClusterSize(10)
+    ec.set_MaxClusterSize(250)
+    ec.set_SearchMethod(tree)
+    cluster_indices = ec.Extract()
 
-    # TODO: Convert PCL data to ROS messages
+	#Assign a color corresponding to each segmented object in scene
+	cluster_color = get_color_list(len(cluster_indices))
+
+	color_cluster_point_list = []
+
+	for j, indices in enumerate(cluster_indices):
+		for i, indice in enumerate(indices):
+			color_cluster_point_list.append([white_cloud[indice][0],
+											white_cloud[indice][1],
+											white_cloud[indice][2],
+											 rgb_to_float(cluster_color[j])])
+
+	#Create new cloud containing all clusters, each with unique color
+	cluster_cloud = pcl.PointCloud_PointXYZRGB()
+	cluster_cloud.from_list(color_cluster_point_list)
+
+    # Convert PCL data to ROS messages
+    ros_cloud_objects =  pcl_to_ros( cloud_objects )
+    ros_cloud_table   =  pcl_to_ros(  cloud_table  )   
+	ros_cluster_cloud =  pcl_to_ros(cluster_cloud)
 
     # Publish ROS messages
-    pcl_objects_pub.publish(pcl_msg)
-    pcl_table_pub.publish(pcl_msg)
+    pcl_objects_pub.publish(ros_cloud_objects)
+    pcl_table_pub.publish(ros_cloud_table)
+	pcl_cluster_pub.publish(ros_cluster_cloud)
 
 
 if __name__ == '__main__':
@@ -40,7 +95,8 @@ if __name__ == '__main__':
 
     # Create Publishers
     pcl_objects_pub = rospy.Publisher("/pcl_objects", PointCloud2, queue, queue_size=1)
-    pcl_table_pub = rospy.Publisher("/pcl_table", PointCloud2, queue, queue_size=1)
+    pcl_table_pub   = rospy.Publisher("/pcl_table",   PointCloud2, queue, queue_size=1)
+	pcl_cluster_pub = rospy.Publisher("/pcl_cluster", PointCloud2, queue, queue_size=1)
 
     # Initialize color_list
     get_color_list.color_list = []
